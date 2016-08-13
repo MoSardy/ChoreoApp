654f07f622b2161b81335f51fa8c4239645891dd
D'sardy <shubhamsardar07@gmail.com>
2016-08-13 18:47:55 +0530

    fairebase setup

--- app/src/main/java/in/dsardy/choreoapp3/MainActivity.java
+++ app/src/main/java/in/dsardy/choreoapp3/MainActivity.java
@@ -1,38 +1,27 @@
 package in.dsardy.choreoapp3;
 
 import android.app.ProgressDialog;
-import android.content.Intent;
 import android.location.Location;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.Window;
-import android.widget.TextView;
-
-import com.yayandroid.locationmanager.LocationBaseActivity;
-import com.yayandroid.locationmanager.LocationConfiguration;
-import com.yayandroid.locationmanager.LocationManager;
-import com.yayandroid.locationmanager.constants.FailType;
-import com.yayandroid.locationmanager.constants.LogType;
-import com.yayandroid.locationmanager.constants.ProviderType;
-
-import java.text.DateFormat;
-import java.util.Date;
 
 
-public class MainActivity extends LocationBaseActivity
-        implements NavigationView.OnNavigationItemSelectedListener{
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener,BulksmsFragment.OnFragmentInteractionListener,ChatroomFragment.OnFragmentInteractionListener,IdeasFragment.OnFragmentInteractionListener,MapFragment.OnFragmentInteractionListener{
 
     public Location mLastLocation = null;
     String mLastUpdateTime;
     private ProgressDialog progressDialog;
-    private TextView locationText;
+    private String locationText;
 
 
     @Override
@@ -43,12 +32,9 @@ public class MainActivity extends LocationBaseActivity
         setContentView(R.layout.activity_main);
 
 
-        //get location
-        LocationManager.setLogType(LogType.GENERAL);
-        getLocation();
 
 
-        locationText = (TextView) findViewById(R.id.locationText);
+
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -62,6 +48,10 @@ public class MainActivity extends LocationBaseActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        // initially go to map frag
+        /*getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragments_container, new MapFragment() ).commit();*/
     }
 
 
@@ -105,12 +95,23 @@ public class MainActivity extends LocationBaseActivity
         int id = item.getItemId();
 
         if (id == R.id.nav_location) {
-            // Handle the camera action
+            // Handle the map action
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragments_container, new MapFragment() ).commit();
         } else if (id == R.id.nav_chatroom) {
+
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragments_container, new ChatroomFragment() ).commit();
 
         } else if (id == R.id.nav_bulksms) {
 
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragments_container, new BulksmsFragment() ).commit();
+
         } else if (id == R.id.nav_ideas) {
+
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragments_container, new IdeasFragment() ).commit();
 
         } else if (id == R.id.nav_share) {
 
@@ -126,100 +127,30 @@ public class MainActivity extends LocationBaseActivity
     //location callbacks
 
 
-    @Override
-    public LocationConfiguration getLocationConfiguration() {
-        return new LocationConfiguration()
-                .keepTracking(true)
-                .askForGooglePlayServices(true)
-                .askForEnableGPS(true)
-                .setMinAccuracy(200.0f)
-                .setWaitPeriod(ProviderType.GOOGLE_PLAY_SERVICES, 5 * 1000)
-                .setWaitPeriod(ProviderType.GPS, 10 * 1000)
-                .setWaitPeriod(ProviderType.NETWORK, 5 * 1000)
-                .setGPSMessage("Would you mind to turn GPS on?")
-                .setRationalMessage("Gimme the permission!");
-    }
 
-    @Override
-    public void onLocationChanged(Location location) {
-        dismissProgress();
-        mLastLocation = location;
-        mLastUpdateTime = DateFormat.getTimeInstance().format(new Date());
-        setText(location);
-    }
 
-    @Override
-    public void onLocationFailed(int failType) {
-        dismissProgress();
 
-        switch (failType) {
-            case FailType.PERMISSION_DENIED: {
-                locationText.setText("Couldn't get location, because user didn't give permission!");
-                break;
-            }
-            case FailType.GP_SERVICES_NOT_AVAILABLE:
-            case FailType.GP_SERVICES_CONNECTION_FAIL: {
-                locationText.setText("Couldn't get location, because Google Play Services not available!");
-                break;
-            }
-            case FailType.NETWORK_NOT_AVAILABLE: {
-                locationText.setText("Couldn't get location, because network is not accessible!");
-                break;
-            }
-            case FailType.TIMEOUT: {
-                locationText.setText("Couldn't get location, and timeout!");
-                break;
-            }
-            case FailType.GP_SERVICES_SETTINGS_DENIED: {
-                locationText.setText("Couldn't get location, because user didn't activate providers via settingsApi!");
-                break;
-            }
-            case FailType.GP_SERVICES_SETTINGS_DIALOG: {
-                locationText.setText("Couldn't display settingsApi dialog!");
-                break;
-            }
-        }
-    }
 
     @Override
     protected void onResume() {
         super.onResume();
 
-        if (getLocationManager().isWaitingForLocation()
-                && !getLocationManager().isAnyDialogShowing()) {
-            displayProgress();
-        }
+
     }
 
     @Override
     protected void onPause() {
         super.onPause();
 
-        dismissProgress();
+
     }
 
-    private void displayProgress() {
-        if (progressDialog == null) {
-            progressDialog = new ProgressDialog(this);
-            progressDialog.getWindow().addFlags(Window.FEATURE_NO_TITLE);
-            progressDialog.setMessage("Getting location...");
-        }
 
-        if (!progressDialog.isShowing()) {
-            progressDialog.show();
-        }
-    }
-
-    private void dismissProgress() {
-        if (progressDialog != null && progressDialog.isShowing()) {
-            progressDialog.dismiss();
-        }
-    }
 
     private void setText(Location location) {
         String appendValue = location.getLatitude() + ", " + location.getLongitude() + "\n";
         String newValue;
-        CharSequence current = locationText.getText();
+        CharSequence current = locationText;
 
         if (!TextUtils.isEmpty(current)) {
             newValue = current + appendValue;
@@ -227,9 +158,12 @@ public class MainActivity extends LocationBaseActivity
             newValue = appendValue;
         }
 
-        locationText.setText(newValue);
+        locationText = newValue;
     }
 
 
+    @Override
+    public void onFragmentInteraction(Uri uri) {
 
+    }
 }
